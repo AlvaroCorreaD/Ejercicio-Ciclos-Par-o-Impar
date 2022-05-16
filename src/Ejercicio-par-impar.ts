84cd87c182ced6e3e76be22b2c8e966d39264791
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-05-15 21:54:09 -0300

    a resolver el ciclo for "Funciona", pero en el ciclo while no me sale el prompt

--- src/Ejercicio-par-impar.ts
+++ src/Ejercicio-par-impar.ts
@@ -1,14 +1,29 @@
-let n: number = Number("");
+// let n: number = Number("");
 
-for (n >= 1; (n = Number); n++) {
-  let n = Number(prompt("Ingrese un número mayor a 0"));
+// for (n >= 1; (n = Number); n++) {
+//   let n = Number(prompt("Ingrese un número mayor a 0"));
+//   if (n === 0) {
+//     alert("Vuelva a ingresar un numero");
+//   } else if (n % 2 == 0) {
+//     console.log("El número " + n + " es par");
+//     break;
+//   } else {
+//     console.log("El número " + n + " es impar");
+//     break;
+//   }
+// }
+
+let n: number;
+
+while (n >= 1) {
+  n = Number(prompt("Ingrese un Número mayor a cero"));
   if (n === 0) {
     alert("Vuelva a ingresar un numero");
-  } else if (n % 2 == 0) {
-    console.log("El número " + n + " es par");
+  } else if (n % 2 === 0) {
+    console.log("El número " + n + " es par.");
     break;
   } else {
-    console.log("El número " + n + " es impar");
+    console.log("El número " + n + " es impar.");
     break;
   }
 }
