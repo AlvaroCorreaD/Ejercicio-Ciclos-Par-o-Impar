9ec34b6c9ce64ca4a2853101364d68291679a57e
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-05-15 22:48:03 -0300

    ...

--- src/Ejercicio-par-impar.ts
+++ src/Ejercicio-par-impar.ts
@@ -1,21 +1,21 @@
-let n: number = Number("");
+// let n: number = Number("");
 
-for (n >= 1; (n = Number); n++) {
-  n = Number(prompt("Ingrese un número mayor a 0"));
-  if (n === 0) {
-    alert("Vuelva a ingresar un numero");
-  } else if (n % 2 == 0) {
-    console.log("El número " + n + " es par");
-    break;
-  } else {
-    console.log("El número " + n + " es impar");
-    break;
-  }
-}
+// for (n >= 1; (n = Number); n++) {
+//   n = Number(prompt("Ingrese un número mayor a 0"));
+//   if (n === 0) {
+//     alert("Vuelva a ingresar un numero");
+//   } else if (n % 2 == 0) {
+//     console.log("El número " + n + " es par");
+//     break;
+//   } else {
+//     console.log("El número " + n + " es impar");
+//     break;
+//   }
+// }
 
 // let n: number;
 
-// while (n > 0) {
+// while (n >= 1) {
 //   n = Number(prompt("Ingrese un Número mayor a cero"));
 //   if (n === 0) {
 //     alert("Vuelva a ingresar un numero");
@@ -27,3 +27,19 @@ for (n >= 1; (n = Number); n++) {
 //     break;
 //   }
 // }
+
+let n: number;
+n = Number("");
+
+while (n > 0) {
+  n = Number(prompt("Ingrese un número mayor a cero."));
+  if (n === 0) {
+    alert("Vuelva a ingresar un número.");
+  } else if (n % 2 === 0) {
+    console.log("El número " + n + " es par.");
+    break;
+  } else {
+    console.log("El número es impar.");
+    break;
+  }
+}
